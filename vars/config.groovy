1b30b99fa06194989ef0047a8801dd4616496caf
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-03 14:13:08 -0300

    fix: yamlpath

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,16 +1,17 @@
 @Library('jenkins-orb')
-import com.jenkins.orb.parser.ConfigParser;
-import com.jenkins.orb.*;
+import com.jenkins.orb.ProjectConfiguration
+import com.jenkins.orb.parser.ConfigParser
 
 def call(String yamlPath) {
-  def yaml = readYaml file: yamlName;
+  def yaml = readYaml file: yamlPath
 
   // load project's configuration
-  ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env);
+  ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env)
 
-  def imageName = projectConfig.dockerConfiguration.imageName().toLowerCase();
+  def imageName = projectConfig.dockerConfiguration.imageName().toLowerCase()
 
   // build the image specified in the configuration
-  def customImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .");
+  def customImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .")
 
+  println "customImage: ${customImage}"
 }
