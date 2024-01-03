4a8b32abca8f66b1a0f54bbc990475b5f785d2f5
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-03 15:00:57 -0300

    test

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,17 +1,18 @@
 @Library('jenkins-orb')
-import com.jenkins.orb.ProjectConfiguration
-import com.jenkins.orb.parser.ConfigParser
+// import com.jenkins.orb.*
 
 def call(String yamlPath) {
   def yaml = readYaml file: yamlPath
+  
+  sh "echo ${yamlPath}"
 
   // load project's configuration
-  ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env)
+  // ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env)
 
-  def imageName = projectConfig.dockerConfiguration.imageName().toLowerCase()
+  // def imageName = projectConfig.dockerConfiguration.imageName().toLowerCase()
 
   // build the image specified in the configuration
-  def customImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .")
+  // def customImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .")
 
-  println "customImage: ${customImage}"
+  // println "customImage: ${customImage}"
 }
