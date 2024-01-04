d8549e9930ad618bd7cfe6eaa9a99c90aca66605
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-04 12:18:59 -0300

    fix: import

--- src/com/jenkins/ci/reference/jobs/Job.groovy
+++ src/com/jenkins/ci/reference/jobs/Job.groovy
@@ -1,6 +1,8 @@
 // src/com/jenkins/ci/reference/jobs/Job.groovy
 package com.jenkins.ci.reference.jobs;
 
+import com.jenkins.ci.reference.commands.Command;
+
 class Job {
     List<Command> steps = [];
     String name;
