9bc45d8eb57d96a2d0165b514ce46704089bfb1f
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-04 12:48:19 -0300

    test: echo

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -8,7 +8,7 @@ def call(Configuration config) {
         jobs.each { job ->
             stage(job.name) {
                 job.steps.each { step ->
-                    echo step
+                    sh "echo ${step}"
                 }
             }
         }
