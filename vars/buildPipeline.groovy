0fe512b37eb3ff1194e92120accc891322974ded
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-04 16:27:29 -0300

    test: echo

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -2,15 +2,26 @@
 import com.jenkins.ci.reference.Configuration
 import com.jenkins.ci.reference.jobs.Job
 
+def generateJob(job) {
+    return {
+        stage(job.name) {
+            job.steps.each { step ->
+                sh step.command
+            }
+        }
+    }
+}
+
 def call(Configuration config) {
     return { variables ->
         List<Job> jobs = config.jobs
         jobs.each { job ->
-            stage(job.name) {
-                job.steps.each { step ->
-                    sh step.command
-                }
-            }
+            generateJob(job)
+            // stage(job.name) {
+            //     job.steps.each { step ->
+            //         sh step.command
+            //     }
+            // }
         }
     
     }
