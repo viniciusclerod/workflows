18d3536f2a2541d580f119157b0cee691b5af107
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-04 10:16:33 -0300

    fix: remove try catch on ci

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,10 +6,5 @@ def call(String yamlPath) {
     def yaml = readYaml file: yamlPath
     Configuration config = ConfigParser.parse(yaml, env)
     def closure = buildPipeline(config)
-    try {
-        closure([:])
-    } catch (exc) {
-        echo 'Something failed, I should sound the klaxons!'
-        throw
-    }
+    closure([:])
 }
