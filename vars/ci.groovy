1c7065243467c52bec2d2a0c038b3685bf0d5c8d
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-05 18:29:32 -0300

    feat: commands

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -3,8 +3,13 @@ import com.jenkins.ci.reference.Configuration
 import com.jenkins.ci.parser.ConfigParser
 
 def call(String yamlPath) {
-    env.PROJECT=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
-    env.TAG=$(git rev-parse --short HEAD)
+    sh '''
+    git_commit=$(git rev-parse --short HEAD)
+    export GIT_COMMIT=$git_commit
+    '''
+
+    // env.PROJECT=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
+    // env.TAG=$(git rev-parse --short HEAD)
 
     def yaml = readYaml file: yamlPath
     Configuration config = ConfigParser.parse(yaml, env)
