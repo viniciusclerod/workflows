afac7d21778846b92c9e1383ff434e727d69cec3
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-07 18:08:26 -0300

    build pipeline

--- vars/buildEnvVars.groovy
+++ vars/buildEnvVars.groovy
@@ -1,7 +1,7 @@
 @Library('jenkins-orb')
 
 def call(Map environment, boolean global = false) {
-    // if (! environment || ! environment.isEmpty()) return [:]
+    if (! environment || ! environment.isEmpty()) return [:]
     // String output = sh(
     //     label: "Preparing environment variables",
     //     script: environment.collect { k, v -> "$k=$v && echo $k=\$$k"}.join('\n'),
