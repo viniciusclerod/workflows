9e0e500eced9b276ddc167cb6e12790cee281365
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-07 10:33:19 -0300

    environemnt

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -5,20 +5,23 @@ import com.jenkins.ci.reference.workflow.Stage
 
 def call(Configuration config) {
     return { variables ->
-        withEnv(buildEnvVars(config.environment, true)) {
-            List<Stage> stgs = config.workflow
-            stgs.each { stg ->
-                if (stg.branches == null || (env.BRANCH_NAME =~ stg.branches).matches()) {
-                    stage(stg.name) {
-                        if (stg.type == 'approval') {
-                            input(message: "Approval is required to proceed.")
-                        } else {
-                            Job job = config.jobs.find { j -> j.name == stg.key }
-                            withEnv(buildEnvVars(job.environment)) {
-                                job.steps.each { step ->
-                                    if (step.type == 'sh') {
-                                        sh script: step.command, returnStdout: true, label: step.name
-                                    }
+        stage("TEST") {
+            withEnv(buildEnvVars(config.environment, true)) {
+                sh "echo $CONTEXT"
+            }
+        }
+        List<Stage> stgs = config.workflow
+        stgs.each { stg ->
+            if (stg.branches == null || (env.BRANCH_NAME =~ stg.branches).matches()) {
+                stage(stg.name) {
+                    if (stg.type == 'approval') {
+                        input(message: "Approval is required to proceed.")
+                    } else {
+                        Job job = config.jobs.find { j -> j.name == stg.key }
+                        withEnv(buildEnvVars(job.environment)) {
+                            job.steps.each { step ->
+                                if (step.type == 'sh') {
+                                    sh script: step.command, returnStdout: true, label: step.name
                                 }
                             }
                         }
