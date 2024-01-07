1bff2238a748ccd36de1370891da4ecccdc06d47
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-07 11:47:31 -0300

    environemnt

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -6,8 +6,8 @@ import com.jenkins.ci.helpers.MapHelper
 
 def call(Configuration config) {
     buildEnvVars(MapHelper.merge([
-        'PROJECT_REPONAME': '$(git config --local remote.origin.url | sed -n \'s#.*/\\([^.]*\\)\\.git#\\1#p\')'
-        'COMMIT_SHA1': '$(git rev-parse HEAD)'
+        'PROJECT_REPONAME': '$(git config --local remote.origin.url | sed -n \'s#.*/\\([^.]*\\)\\.git#\\1#p\')',
+        'COMMIT_SHA1': '$(git rev-parse HEAD)',
         'COMMIT_SHA1_SHORT': '$(git rev-parse --short HEAD)'
     ], config.environment), true)
     return { variables ->
