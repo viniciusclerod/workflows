b40a8c1c03c7cba34e296b1ca5cc70ed4c1a56e7
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-07 18:19:52 -0300

    build pipeline

--- vars/buildEnvVars.groovy
+++ vars/buildEnvVars.groovy
@@ -2,6 +2,11 @@
 
 def call(Map environment, boolean global = false) {
     echo environment.getClass().getName()
+    if (environment.isEmpty()) {
+        echo "environment is empty"
+    } else {
+        echo "environment is not empty"
+    }
     // if (! environment || ! environment.isEmpty()) return [:]
     String output = sh(
         label: "Preparing environment variables",
