abff4fc41c9c7cacf3f2f7e5d1d7f34550af295d
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-07 13:59:05 -0300

    environemnt

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -5,19 +5,17 @@ import com.jenkins.ci.reference.workflow.Stage
 
 def call(Stage stg, Configuration config) {
     echo "${stg.branches} ${env.BRANCH_NAME}"
-    return { variables ->
-        if (stg.branches == null || (env.BRANCH_NAME =~ stg.branches).matches()) {
-            echo "BUILD Stage"
-            stage(stg.name) {
-                if (stg.type == 'approval') {
-                    input(message: "Approval is required to proceed.")
-                } else {
-                    Job job = config.jobs.find { j -> j.name == stg.key }
-                    withEnv(buildEnvVars(job.environment)) {
-                        job.steps.each { step ->
-                            if (step.type == 'sh') {
-                                sh script: step.command, returnStdout: true, label: step.name
-                            }
+    if (stg.branches == null || (env.BRANCH_NAME =~ stg.branches).matches()) {
+        echo "BUILD Stage"
+        stage(stg.name) {
+            if (stg.type == 'approval') {
+                input(message: "Approval is required to proceed.")
+            } else {
+                Job job = config.jobs.find { j -> j.name == stg.key }
+                withEnv(buildEnvVars(job.environment)) {
+                    job.steps.each { step ->
+                        if (step.type == 'sh') {
+                            sh script: step.command, returnStdout: true, label: step.name
                         }
                     }
                 }
