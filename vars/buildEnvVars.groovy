3b67443972856c109ccb224a8f0c219771b8c80f
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-07 18:37:39 -0300

    build pipeline

--- vars/buildEnvVars.groovy
+++ vars/buildEnvVars.groovy
@@ -7,12 +7,11 @@ def call(Map environment, boolean global = false) {
         script: environment.collect { k, v -> "$k=$v && echo $k=\$$k"}.join('\n'),
         returnStdout: true
     ).trim()
-    return ['MOCK=value']
-    // return output.split('\n').collect { it ->
-    //     if (global) {
-    //         def (k, v) = it.split('=', 2)
-    //         env.setProperty(k, v)
-    //     }
-    //     return it
-    // }
+    return output.split('\n').collect { it ->
+        if (global) {
+            def (k, v) = it.split('=', 2)
+            env.setProperty(k, v)
+        }
+        return it
+    }
 }
