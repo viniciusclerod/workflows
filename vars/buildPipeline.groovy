6b0e1330b1795402b5632303cd7a17dcf62716be
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-07 13:50:19 -0300

    environemnt

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,8 +1,7 @@
 @Library('jenkins-orb')
-import com.jenkins.ci.reference.Configuration
-import com.jenkins.ci.reference.jobs.Job
-import com.jenkins.ci.reference.workflow.Stage
 import com.jenkins.ci.helpers.MapHelper
+import com.jenkins.ci.reference.Configuration
+import com.jenkins.ci.reference.workflow.Stage
 
 def call(Configuration config) {
     buildEnvVars(MapHelper.merge([
@@ -13,23 +12,6 @@ def call(Configuration config) {
     ], config.environment), true)
     return { variables ->
         List<Stage> stgs = config.workflow
-        stgs.each { stg ->
-            if (stg.branches == null || (env.BRANCH_NAME =~ stg.branches).matches()) {
-                stage(stg.name) {
-                    if (stg.type == 'approval') {
-                        input(message: "Approval is required to proceed.")
-                    } else {
-                        Job job = config.jobs.find { j -> j.name == stg.key }
-                        withEnv(buildEnvVars(job.environment)) {
-                            job.steps.each { step ->
-                                if (step.type == 'sh') {
-                                    sh script: step.command, returnStdout: true, label: step.name
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        stgs.each { stg -> buildStage(stg, config) }
     }
 }
---
+++ vars/buildStage.groovy
@@ -0,0 +1,25 @@
+@Library('jenkins-orb')
+import com.jenkins.ci.reference.Configuration
+import com.jenkins.ci.reference.jobs.Job
+import com.jenkins.ci.reference.workflow.Stage
+
+def call(Stage stg, Configuration config) {
+    return { variables ->
+        if (stg.branches == null || (env.BRANCH_NAME =~ stg.branches).matches()) {
+            stage(stg.name) {
+                if (stg.type == 'approval') {
+                    input(message: "Approval is required to proceed.")
+                } else {
+                    Job job = config.jobs.find { j -> j.name == stg.key }
+                    withEnv(buildEnvVars(job.environment)) {
+                        job.steps.each { step ->
+                            if (step.type == 'sh') {
+                                sh script: step.command, returnStdout: true, label: step.name
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
