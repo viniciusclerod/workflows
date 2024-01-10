8af500ec8086445085cfdca4f6c48a8a4b12be3a
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-09 21:28:45 -0300

    filters

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -6,33 +6,24 @@ import com.jenkins.ci.reference.Filter
 
 def call(Stage stg, Configuration config) {
     if (shouldRun(stg.filters)) {
-        stage("TEST") {
-            sh(
-                label: "Teste customizado",
-                script: "echo $VERSION",
-                returnStdout: true
-            )
+        stage(stg.name) {
+            if (stg.type == 'approval') {
+                input(message: "Approval is required to proceed.")
+            } else {
+                Job job = config.jobs.find { j -> j.name == stg.key }
+                withEnv(buildEnvironment(job.environment)) {
+                    job.steps.each { step ->
+                        if (step.type == 'sh') {
+                            sh(
+                                label: step.name,
+                                script: step.command,
+                                returnStdout: true
+                            )
+                        }
+                    }
+                }
+            }
         }
-    //     stage(stg.name) {
-    //         if (stg.type == 'approval') {
-    //             input(message: "Approval is required to proceed.")
-    //         } else {
-    //             Job job = config.jobs.find { j -> j.name == stg.key }
-    //             withEnv(buildEnvironment(job.environment)) {
-    //                 job.steps.each { step ->
-    //                     if (step.type == 'sh') {
-    //                         ansiColor('xterm') {
-    //                             sh(
-    //                                 label: step.name,
-    //                                 script: step.command,
-    //                                 returnStdout: true
-    //                             )
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
     }
 }
     
