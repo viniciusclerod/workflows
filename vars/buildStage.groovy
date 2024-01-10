dae0f39c4ce46aa7133bdd745d782c5c7e34e0f4
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-10 14:06:25 -0300

    filters

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -31,16 +31,25 @@ Boolean shouldRun(Map filters) {
     echo "BRANCH_NAME=${env.BRANCH_NAME}\nCHANGE_BRANCH=${env.CHANGE_BRANCH}\nCHANGE_ID=${env.CHANGE_ID}"
     if (filters) {
         if (filters.pull_request) {
-            Boolean hasPrOpened = (env.CHANGE_ID != null)
-            if (!hasPrOpened) return false
-        }
-        if (filters.branches && filters.branches.ignore) {
-            Boolean shouldBeIgnored = (env.BRANCH_NAME =~ filters.branches.ignore).matches()
-            return !shouldBeIgnored
-        }
-        if (filters.branches && filters.branches.only) {
-            Boolean shouldBeConsiderated = (env.BRANCH_NAME =~ filters.branches.only).matches()
-            return shouldBeConsiderated
+            // Boolean hasPrOpened = (env.CHANGE_ID != null)
+            // if (!hasPrOpened) return false
+            if (filters.branches && filters.branches.ignore) {
+                Boolean shouldBeIgnored = (env.CHANGE_BRANCH =~ filters.branches.ignore).matches()
+                return !shouldBeIgnored
+            }
+            if (filters.branches && filters.branches.only) {
+                Boolean shouldBeConsiderated = (env.CHANGE_BRANCH =~ filters.branches.only).matches()
+                return shouldBeConsiderated
+            }
+        } else {
+            if (filters.branches && filters.branches.ignore) {
+                Boolean shouldBeIgnored = (env.BRANCH_NAME =~ filters.branches.ignore).matches()
+                return !shouldBeIgnored
+            }
+            if (filters.branches && filters.branches.only) {
+                Boolean shouldBeConsiderated = (env.BRANCH_NAME =~ filters.branches.only).matches()
+                return shouldBeConsiderated
+            }
         }
     }
     return true
