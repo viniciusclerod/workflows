ba0286c0541efd48879b1c7156d222a8a346a471
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-10 17:15:14 -0300

    filters

--- src/com/jenkins/ci/parser/ConfigParser.groovy
+++ src/com/jenkins/ci/parser/ConfigParser.groovy
@@ -34,11 +34,11 @@ class ConfigParser {
                             type: 'sh',
                             name: 'Shell Script'
                         )
-                        if (it[run] instanceof String) {
-                            step.command = step.run
+                        if (it.run] instanceof String) {
+                            step.command = it.run
                         } else {
-                            step.name = it[run].name ?: step.name
-                            step.command = it[run].command
+                            step.name = it.run].name ?: step.name
+                            step.command = it.run].command
                         }
                         return step
                 }
