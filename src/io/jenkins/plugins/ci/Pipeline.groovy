9542c5656235133a86ee21adb59a4829f03afc94
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-12 13:49:44 -0300

    test: build executor

--- src/io/jenkins/plugins/ci/Pipeline.groovy
+++ src/io/jenkins/plugins/ci/Pipeline.groovy
@@ -7,22 +7,21 @@ class Pipeline {
     // Pipeline(String yamlPath) {}
 
     def execute(def ctx) {
+        this.buildNode()
+    }
+
+    def buildNode(def ctx) {
         def closure = {
-            this.buildNode()
+            node {
+                stage('Setup') {
+                    echo "Executed (ctx is ${ctx.getClass()} class)"
+                    checkout scm
+                }
+                //   ci '.jenkins/config.yaml'
+            }
         }
         closure.delegate = ctx
         closure.call()
-        
-    }
-
-    def buildNode() {
-        node {
-            stage('Setup') {
-                echo "Executed (ctx is ${ctx.getClass()} class)"
-                checkout scm
-            }
-            //   ci '.jenkins/config.yaml'
-        }
     }
 
 }
