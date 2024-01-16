753e74f3091c07ab5a1fb67952dcb7ee335c456a
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-16 09:27:38 -0300

    test: parse commands args

--- src/io/jenkins/plugins/ci/model/Command.groovy
+++ src/io/jenkins/plugins/ci/model/Command.groovy
@@ -15,11 +15,16 @@ class Command {
       if (this.steps.isEmpty()) {
         this.invoke(arguments)
       } else {
-        this.steps.each { step ->
-          step.execute()
+        this.steps.each { source ->
+          Step target = new Step(source.properties.findAll { it.key != 'class' })
+          target.arguments.each {
+            target.arguments[it.key] = this.parseAttrs([
+                parameters: this.getMergedArgs(arguments)
+            ], it.value)
+          }
+          target.execute()
         }
       }
-
     }
 
     def invoke(def arguments) {
@@ -32,6 +37,26 @@ class Command {
           context = this.context
       }
       context.invokeMethod(this.name, arguments)
+    }
+
+    def getMergedArgs(Map arguments = [:]) {
+        Map defaultArgs = this.parameters.collectEntries { key, value ->
+            ["${key}": value.default]
+        }.findAll { it.value != null } ?: [:]
+        Map externalArgs = arguments.collectEntries { key, value ->
+            String type = value.getClass().getSimpleName().toLowerCase()
+            return ["${key}": (type == this.parameters[key].type) ? value : null]
+        }.findAll { it.value != null } ?: [:]
+        return MapHelper.merge(defaultArgs, externalArgs)
+    }
+
+    def parseAttrs(def context = this, String text) {
+        return text.replaceAll(/<<\s*([\S]+)\s*>>/) { match ->
+            def keys = match[1].split("\\.")
+            def value = context
+            keys.each { key -> value = value[key] }
+            return value
+        }
     }
 
 }
