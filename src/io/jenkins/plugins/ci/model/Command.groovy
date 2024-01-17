7bf20443dfcaa3a8b5a1f8982c1dc205c82242b1
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-17 17:56:27 -0300

    fix: command args

--- src/io/jenkins/plugins/ci/model/Command.groovy
+++ src/io/jenkins/plugins/ci/model/Command.groovy
@@ -48,58 +48,9 @@ class Command {
           //     return ["${key}": (type == this.parameters[key]?.type) ? value : null]
           // }.findAll { it.value != null } ?: [:]
           return MapHelper.merge(parameters, arguments, { a, b -> b ?: a })
-        // case NullObject:
-        //   if (defaultArgs) return defaultArgs
-        //   break
-        // case String:
         default:
           return arguments
       }
     }
-    
-    // def execStep(Step source, def arguments) {
-    //   // Step target = new Step(source.properties.findAll { it.key != 'class' }) // COPY
-
-    //   // target.arguments.each {
-    //   //   target.arguments[it.key] = this.parseAttrs([
-    //   //       parameters: this.getMergedArgs(arguments)
-    //   //   ], it.value)
-    //   // }
-    //   // target.arguments = this.getMergedArgs(arguments)
-    //   // target.execute()
-    // }
-
-    // def getMergedArgs(def arguments) {
-    //   this.ctx.echo "arguments=${arguments} as ${arguments.getClass()}"
-    //   Map defaultArgs = this.parameters.collectEntries { key, value ->
-    //       ["${key}": value.default]
-    //   }.findAll { it.value != null } ?: [:]
-    //   switch (arguments) {
-    //     case Map:
-    //       // Map externalArgs = arguments.collectEntries { key, value ->
-    //       //     String type = value.getClass().getSimpleName().toLowerCase()
-    //       //     return ["${key}": (type == this.parameters[key]?.type) ? value : null]
-    //       // }.findAll { it.value != null } ?: [:]
-    //       if (defaultArgs) return MapHelper.merge(defaultArgs, arguments)
-    //       break
-    //     case NullObject:
-    //       if (defaultArgs) return defaultArgs
-    //       break
-    //     case String:
-    //     default:
-    //       // TODO
-    //       break
-    //   }
-    //   return arguments
-    // }
-
-    // def parseAttrs(def context = this, String text) {
-    //     return text.replaceAll(/<<\s*([\S]+)\s*>>/) { match ->
-    //         def keys = match[1].split("\\.")
-    //         def value = context
-    //         keys.each { key -> value = value[key] }
-    //         return value
-    //     }
-    // }
 
 }
--- src/io/jenkins/plugins/ci/model/Step.groovy
+++ src/io/jenkins/plugins/ci/model/Step.groovy
@@ -5,18 +5,13 @@ import io.jenkins.plugins.ci.model.Command
 
 class Step {
 
-    def ctx // TODO: REMOVE
-
     Command command
     def arguments
 
     def execute(def parameters = null, def ctx = null) {
         def arguments = this.arguments
         if (parameters) {
-            if (ctx) this.ctx = ctx // TODO: REMOVE
-            // if (this.ctx) this.ctx.echo "BEGIN arguments=${arguments} parameters=${parameters}" // TODO: REMOVE
             arguments = this.parseArguments([ parameters: parameters ], arguments)
-            // if (this.ctx) this.ctx.echo "END arguments=${arguments} parameters=${parameters}" // TODO: REMOVE
         }
         this.command.execute(arguments)
     }
@@ -26,7 +21,6 @@ class Step {
             case Map:
                 return arguments.collectEntries { it ->
                     def value = it.value instanceof String ? this.parseArgument(context, it.value) : it.value
-                    if (this.ctx) this.ctx.echo "context=${context} it.value=${it.value} value=${value}" // TODO: REMOVE
                     return [(it.key): value]
                 }
             case String:
