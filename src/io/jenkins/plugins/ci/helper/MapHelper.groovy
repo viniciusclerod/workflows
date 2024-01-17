a4864fc74a2f30ece37720eda155a0e3fefb6b73
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-17 12:00:51 -0300

    fix: command args

--- src/io/jenkins/plugins/ci/helper/MapHelper.groovy
+++ src/io/jenkins/plugins/ci/helper/MapHelper.groovy
@@ -2,7 +2,7 @@ package io.jenkins.plugins.ci.helper
 
 class MapHelper {
     static Map merge(Map map1, Map map2, Closure cls = { a, b -> a + b }) {
-        (map1.keySet() + map2.keySet()).collectEntries {
+        (map1.keySet().plus(map2.keySet()).unique()).collectEntries {
             [(it): map1[it] && map2[it] ? cls.call(map1[it], map2[it]) : (map1[it] ?: map2[it])]
         }
     }
