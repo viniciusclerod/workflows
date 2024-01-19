962c7d4ba8ed2a62f88d6d1241ad2ca662d12071
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-19 13:58:05 -0300

    feat: script execution

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,11 +1,12 @@
 @Library('jenkins-orb')
 
 def call(String script) {
-    echo "${this} ${this.getClass()}"
-    def closure = {
-        return Eval.me(script)
-    }
-    closure.delegate = this
-    closure.resolveStrategy = Closure.DELEGATE_FIRST
-    return closure.call()
+    echo "${this.echo}"
+    echo evaluate(script)
+    // def closure = {
+    //     return Eval.me(script)
+    // }
+    // closure.delegate = this
+    // closure.resolveStrategy = Closure.DELEGATE_FIRST
+    // return closure.call()
 }
