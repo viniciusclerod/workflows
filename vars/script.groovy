6bfce7944059e82af62cc6f0c17ece3c156d0883
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-19 11:01:16 -0300

    feat: script execution

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,12 +1,11 @@
 @Library('jenkins-orb')
 
-@Grab('org.codehaus.groovy:groovy-all:2.4.7')
-import groovy.lang.GroovyClassLoader
 
 def call(String script) {
     // def shell = new GroovyShell()
     // shell.evaluate(script)
-    def classLoader = new GroovyClassLoader()
-    def clazz = classLoader.parseClass(script)
-    clazz.newInstance()
+    // def classLoader = new GroovyClassLoader()
+    // def clazz = classLoader.parseClass(script)
+    // clazz.newInstance()
+    Eval.me(script)
 }
