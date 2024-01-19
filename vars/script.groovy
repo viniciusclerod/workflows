115466bf88abbe47fbefc252d43033cab48fa493
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-19 12:30:56 -0300

    feat: script execution

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,7 +1,8 @@
 @Library('jenkins-orb')
 
 def call(String script) {
-    echo "${this}"
+    def methods = this.getClass().getMethods().collect { it.name }
+    echo "${this.getClass()}\n${methods}"
     def closure = {
         return Eval.me(script)
     }
