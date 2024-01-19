ff14c71e2711835f576ff4ee1e1dc1cb8cc4350f
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-19 15:08:47 -0300

    feat: environment

--- src/io/jenkins/plugins/ci/Pipeline.groovy
+++ src/io/jenkins/plugins/ci/Pipeline.groovy
@@ -34,7 +34,8 @@ class Pipeline {
                 def yaml = readYaml file: this.yamlPath
                 this.config = ConfigParser.parse(ctx, yaml)
                 if (this.config.environment) {
-                    List<String> environment = this.getEnvironment(ctx)
+                    echo this.config.environment
+                    // List<String> environment = this.getEnvironment(ctx)
                     // environment.each { env ->
                     //     def (key, value) = env.split('=', 2)
                     //     env.setProperty(key, value)
@@ -58,13 +59,13 @@ class Pipeline {
         script.call()
     }
 
-    def getEnvironment(def ctx) {
-        String output = ctx.sh(
-            label: "Preparing environment variables",
-            script: this.config.environment.collect { k, v -> "$k=$v && echo $k=\$$k"}.join('\n'),
-            returnStdout: true
-        ).trim()
-        return output.split('\n')
-    }
+    // def getEnvironment(def ctx) {
+    //     String output = ctx.sh(
+    //         label: "Preparing environment variables",
+    //         script: this.config.environment.collect { k, v -> "$k=$v && echo $k=\$$k"}.join('\n'),
+    //         returnStdout: true
+    //     ).trim()
+    //     return output.split('\n')
+    // }
 
 }
