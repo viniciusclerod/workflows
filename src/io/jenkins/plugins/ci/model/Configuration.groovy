731cc664db5e7d1da639f65e1943c10958ccb80f
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-19 15:10:24 -0300

    feat: environment

--- src/io/jenkins/plugins/ci/model/Configuration.groovy
+++ src/io/jenkins/plugins/ci/model/Configuration.groovy
@@ -4,6 +4,8 @@ import io.jenkins.plugins.ci.model.Command
 import io.jenkins.plugins.ci.model.Step
 
 class Configuration {
+    
+    Map environment = [:]
     Map<String,Command> commands = [:]
     List<Step> steps = []
 }
