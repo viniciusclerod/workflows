96b76f7cf571c5f15f4ac9eca3e30419f1c14111
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-23 11:24:04 -0300

    feat: workflows

--- src/io/jenkins/plugins/ci/parser/ConfigParser.groovy
+++ src/io/jenkins/plugins/ci/parser/ConfigParser.groovy
@@ -69,10 +69,10 @@ class ConfigParser {
     static void buildWorkflows(def ctx, Configuration config, Map map) {
         map.each { key, value ->
             ctx.echo "${key}=${value}"
-            // Workflow workflow = new Workflow(
-            //     name: value?.name ?: key,
-            //     stages: ConfigParser.parseStages(ctx, config, value.jobs) ?: []
-            // )
+            Workflow workflow = new Workflow(
+                name: value?.name ?: key
+                // stages: ConfigParser.parseStages(ctx, config, value.jobs) ?: []
+            )
             // config.workflows[key] = workflow
         }
     }
