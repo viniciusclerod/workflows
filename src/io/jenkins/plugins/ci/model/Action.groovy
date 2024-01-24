f05fa52b575560e61a055f7a429b9160f1c11310
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-24 08:53:16 -0300

    feat: workflows

--- src/io/jenkins/plugins/ci/model/Action.groovy
+++ src/io/jenkins/plugins/ci/model/Action.groovy
@@ -1,13 +1,14 @@
 package io.jenkins.plugins.ci.model
 
 import io.jenkins.plugins.ci.model.Job
+import io.jenkins.plugins.ci.model.Filter
 
 class Action {
 
     String name
     String type
     Job job
-    Map filters
+    Map<String,Filter> filters
 
     def execute() {
         this.job.execute()
