2e2d71b24b19ffe75bcdb531aa2ad6781044d049
Vinícius Rodrigues <jclemente@uolinc.com>
2024-01-24 08:49:28 -0300

    feat: workflows

--- src/io/jenkins/plugins/ci/model/Configuration.groovy
+++ src/io/jenkins/plugins/ci/model/Configuration.groovy
@@ -2,12 +2,13 @@ package io.jenkins.plugins.ci.model
 
 import io.jenkins.plugins.ci.model.Command
 import io.jenkins.plugins.ci.model.Step
+import io.jenkins.plugins.ci.model.Workflow
 
 class Configuration {
     
     Map environment = [:]
     Map<String,Command> commands = [:]
     Map<String,Job> jobs = [:]
-    Map<String,Workflows> workflows = [:]
+    Map<String,Workflow> workflows = [:]
 
 }
