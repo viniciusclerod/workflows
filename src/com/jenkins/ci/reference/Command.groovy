16126221f4fa892a033992be7e75b7a3f5fcf12b
Vinícius Rodrigues <jclemente@uolinc.com>
2024-02-05 14:37:46 -0300

    fix: collect entries on command

--- src/com/jenkins/ci/reference/Command.groovy
+++ src/com/jenkins/ci/reference/Command.groovy
@@ -14,6 +14,7 @@ class Command {
 
     def call(Map arguments = [:]) {
         if (this.steps.isEmpty()) {
+            this.context.echo "arguments=${arguments}"
             def params = this.getParams(arguments)
             this.context.echo "params=${params}"
             this.context.invokeMethod(this.name, params)
