427ea0430a8afd7002df62b0013031df596ce0ca
Vinícius Rodrigues <jclemente@uolinc.com>
2024-02-26 21:30:49 -0300

    feat: debug

--- src/io/jenkins/plugins/workflows/Pipeline.groovy
+++ src/io/jenkins/plugins/workflows/Pipeline.groovy
@@ -68,30 +68,28 @@ class Pipeline {
     def buildActions(def ctx, List actions) {
         def script = {
             actions.each { action ->
-                try {
-                    if (action.shouldRun(ctx.env)) {
-                        stage(action.name) {
-                            if (action.type == 'approval') {
-                                input(message: "Approval is required to proceed.")
-                            } else {
-                                withCredentials(
-                                    this.processCredentials(ctx, action.getContextCredentials())
-                                ) {
-                                    withEnv(this.processEnvironment(ctx, MapHelper.merge(
-                                        action.getContextEnvironment(),
-                                        action.job.environment
-                                    ))) {
+                if (action.shouldRun(ctx.env)) {
+                    stage(action.name) {
+                        if (action.type == 'approval') {
+                            input(message: "Approval is required to proceed.")
+                        } else {
+                            withCredentials(
+                                this.processCredentials(ctx, action.getContextCredentials())
+                            ) {
+                                withEnv(this.processEnvironment(ctx, MapHelper.merge(
+                                    action.getContextEnvironment(),
+                                    action.job.environment
+                                ))) {
+                                    try {
                                         action.execute()
+                                    } catch (Exception e) {
+                                        echo "Failed to parse action: ${e}"
+                                        echo "Action: ${action.properties}"
                                     }
                                 }
                             }
                         }
                     }
-                } catch (Exception e) {
-                    echo "Failed to parse action: ${e}"
-                    echo "${e.properties}"
-                    echo "${e.stackTrace}"
-                    echo "Action: ${action.properties}"
                 }
             }
         }
@@ -100,22 +98,32 @@ class Pipeline {
     }
 
     List processCredentials(def ctx, List credentials) {
-        if (!credentials as Boolean) return []
-        return credentials.collect { credential ->
-            ctx.invokeMethod(credential.type, credential.parameters)
+        try {
+            if (!credentials as Boolean) return []
+            return credentials.collect { credential ->
+                ctx.invokeMethod(credential.type, credential.parameters)
+            }
+        } catch (Exception e) {
+            ctx.echo "Failed to process credentials: ${e}"
+            return []
         }
     }
 
     List<String> processEnvironment(def ctx, Map environment = this.config.environment) {
-        if (!environment as Boolean) return []
-        String script = '#!/usr/bin/env bash\n' << 
-            environment.collect { k, v -> "$k=$v\necho $k=\$$k" }.join('\n') as String
-        String output = ctx.sh(
-            label: "Preparing environment variables",
-            script: script,
-            returnStdout: true
-        ).trim()
-        return output.split('\n') as List<String>
+        try {
+            if (!environment as Boolean) return []
+            String script = '#!/usr/bin/env bash\n' << 
+                environment.collect { k, v -> "$k=$v\necho $k=\$$k" }.join('\n') as String
+            String output = ctx.sh(
+                label: "Preparing environment variables",
+                script: script,
+                returnStdout: true
+            ).trim()
+            return output.split('\n') as List<String>
+        } catch (Exception e) {
+            ctx.echo "Failed to process environment: ${e}"
+            return []
+        }
     }
 
 }
